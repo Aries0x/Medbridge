06bec3286a31d2318aba76744bf1bd04c1d2925c
Sunil kumar <matrixbreacher07@gmail.com>
2026-04-25 20:02:38 +0530

    Add mergekit back with --no-deps (required by TRL GRPO import)

--- training/train.py
+++ training/train.py
@@ -26,6 +26,7 @@ Usage:
 # %% Install dependencies
 # !pip install -q "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 # !pip install -q --no-deps "trl>=0.12.0"
+# !pip install -q --no-deps mergekit  # TRL GRPO needs this at import time
 # !pip install -q openenv-core datasets accelerate bitsandbytes wandb "transformers>=4.45.0" peft sentencepiece
 # !pip install -q textstat langdetect  # For local reward calculation
 # !pip install -q git+https://github.com/Aries0x/Medbridge.git  # MedBridge client + rewards
